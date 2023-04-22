46936198f8168af2285838014972e0d105bc5068
Dilson82 <131519041+Dilson82@users.noreply.github.com>
2023-04-22 19:27:23 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+        function sendEmail() {
+          Email.send({
+          Host : "smtp.elasticmail.com",
+          Username : "wilson.dallas82@gmail.com",
+          Password : "Dwilson1982",
+          To : 'wilson.dallas82@gmail.com',
+          From : "wilson.dallas82@gmail.com",
+          Subject : "New Enquiry",
+          Body : "Test",  
+          }).then(
+              message => alert("Thank you for your Enquiry")
+          );
+        }
